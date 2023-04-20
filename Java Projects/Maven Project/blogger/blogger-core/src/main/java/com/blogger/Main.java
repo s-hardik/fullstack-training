4fe74927b01ef29baa4fe7df75527238fefb74e1
hardik shah <shardik205@gmail.com>
2023-04-20 20:04:16 +0530

    maven multimodule project by intelij IDE

---
+++ Java Projects/Maven Project/blogger/blogger-core/src/main/java/com/blogger/Main.java
@@ -0,0 +1,10 @@
+package com.blogger;
+
+import com.blogger.util.Utility;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        System.out.println(Utility.isNull(null));
+    }
+}
