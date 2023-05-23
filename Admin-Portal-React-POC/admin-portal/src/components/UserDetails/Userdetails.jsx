c30e625115f0c252041aa0b9d4c9804e8aebfb29
hardik shah <shardik205@gmail.com>
2023-05-23 17:32:37 +0530

    Added UI for Employee User page

--- Admin-Portal-React-POC/admin-portal/src/components/UserDetails/Userdetails.jsx
+++ Admin-Portal-React-POC/admin-portal/src/components/UserDetails/Userdetails.jsx
@@ -4,7 +4,40 @@ import { useEffect } from 'react'
 import {getEmployeeByEmailId} from '../../service/EmployeeService';
 import { logout } from "../../service/AuthService";
 import logo from "../../assets/logo.png";
+import userImg from "../../assets/user.jpg";
+import { styled } from '@mui/material/styles';
+import Card from '@mui/material/Card';
+import CardHeader from '@mui/material/CardHeader';
+import CardMedia from '@mui/material/CardMedia';
+import CardContent from '@mui/material/CardContent';
+import CardActions from '@mui/material/CardActions';
+import Collapse from '@mui/material/Collapse';
+import Avatar from '@mui/material/Avatar';
+import IconButton from '@mui/material/IconButton';
+import Typography from '@mui/material/Typography';
+import { red } from '@mui/material/colors';
+import FavoriteIcon from '@mui/icons-material/Favorite';
+import ShareIcon from '@mui/icons-material/Share';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import MoreVertIcon from '@mui/icons-material/MoreVert';
+
+const ExpandMore = styled((props) => {
+  const { expand, ...other } = props;
+  return <IconButton {...other} />;
+})(({ theme, expand }) => ({
+  transform: !expand ? 'rotate(0deg)' : 'rotate(180deg)',
+  marginLeft: 'auto',
+  transition: theme.transitions.create('transform', {
+    duration: theme.transitions.duration.shortest,
+  }),
+}));
 const Userdetails = ({user}) => {
+    const [expanded, setExpanded] = React.useState(false);
+
+    const handleExpandClick = () => {
+      setExpanded(!expanded);
+    };
+  
     const [empDetails, setEmpDetails] = useState({});
     const logoutHandler = ()=>{
         logout();
@@ -39,11 +72,60 @@ const Userdetails = ({user}) => {
         <div className="dashboard-heading">
           <h3>Your Details</h3>
         </div>
-        <div>
-            {
-                empDetails? JSON.stringify(empDetails) : <p>No Details Found. Please Check With Admin!</p>
-            }
-        </div>
+        <Card sx={{ maxWidth: 345 }}>
+      <CardHeader
+        avatar={
+          <Avatar sx={{ bgcolor: red[500] }} aria-label="employee">
+          </Avatar>
+        }
+        action={
+          <IconButton aria-label="settings">
+            <MoreVertIcon />
+          </IconButton>
+        }
+        title={empDetails.firstName}
+        subheader={empDetails.empId}
+      />
+      <CardMedia
+        component="img"
+        height="194"
+        image={userImg}
+        alt="Paella dish"
+      />
+      <CardContent>
+        <Typography variant="body2" color="text.secondary">
+        <p><b>First Name:</b> {empDetails.firstName}</p>
+        <p><b>Middle Name:</b>  {empDetails.middleName}</p>
+        <p><b>Last Name: </b> {empDetails.lastName}</p>
+        </Typography>
+      </CardContent>
+      <CardActions disableSpacing>
+        <IconButton aria-label="share">
+          <ShareIcon />
+        </IconButton>
+        <ExpandMore
+          expand={expanded}
+          onClick={handleExpandClick}
+          aria-expanded={expanded}
+          aria-label="show more"
+        >
+          <ExpandMoreIcon />
+        </ExpandMore>
+      </CardActions>
+      <Collapse in={expanded} timeout="auto" unmountOnExit>
+        <CardContent>
+        <Typography variant="body2" color="text.secondary">
+        <p><b>Email Id:</b> {empDetails.email}</p>
+        <p><b>Contact Number:</b>  {empDetails.contactNumber}</p>
+        <p><b>Emergency Contact Number:</b>  {empDetails.emergencyNumber}</p>
+        <p><b>PAN Number:</b>  {empDetails.panNo}</p>
+        <p><b>Blood Group:</b>  {empDetails.bloodGroup}</p>
+        <p><b>Permanent Address:</b>  {empDetails.permanentAddress}</p>
+        <p><b>Present Address:</b>  {empDetails.presentAddress}</p>
+        </Typography>
+        </CardContent>
+      </Collapse>
+    </Card>
       </main>
     </>
   )
